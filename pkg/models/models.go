35c6b667c9f8e8aad453eddd9526637fa976a389
berkayersoy <berkay.ersoy.007@gmail.com>
2022-01-26 03:41:43 +0300

    created

---
+++ pkg/models/models.go
@@ -0,0 +1,8 @@
+package models
+
+type Product struct {
+	Id          int     `json:"id"`
+	Name        string  `json:"name"`
+	Price       float32 `json:"price"`
+	Description string  `json:"description"`
+}
---
+++ web/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/berkay.ersoyy/go-products-example/pkg/models"
+)
+
+var products = []models.Product{
+	{Id: 1, Name: "Monster", Price: 25.0, Description: "Abra a7"},
+	{Id: 2, Name: "Mac", Price: 55.0, Description: "M1"},
+	{Id: 3, Name: "Lenovo", Price: 15.0, Description: "Gaming"},
+}
+
+func main() {
+
+}
