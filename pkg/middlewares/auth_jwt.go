6ad104659ea3870f3e262219a7ceb29d7d8f31eb
berkayersoy <berkay.ersoy.007@gmail.com>
2022-01-31 16:26:51 +0300

    simple jwt bugs fixed

--- pkg/middlewares/auth_jwt.go
+++ pkg/middlewares/auth_jwt.go
@@ -13,8 +13,10 @@ func AuthorizeJWT(a services.AuthService) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		const BEARER_SCHEMA = "Bearer"
 		authHeader := c.GetHeader("Authorization")
-		tokenString := authHeader[len(BEARER_SCHEMA):]
+		tokenString := authHeader[len(BEARER_SCHEMA)+1:]
+		fmt.Println("1", tokenString)
 		token, err := a.ValidateToken(tokenString)
+		fmt.Println("2", tokenString)
 		if token.Valid {
 			claims := token.Claims.(jwt.MapClaims)
 			fmt.Println(claims)
--- pkg/services/auth_service.go
+++ pkg/services/auth_service.go
@@ -18,19 +18,21 @@ func (a *AuthService) CreateToken(userId uint) (string, error) {
 	atClaims["user_id"] = userId
 	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
-	token, err := at.SignedString([]byte("cilginkovboy12"))
+	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
 	if err != nil {
 		return "", err
 	}
 	return token, nil
 }
-func (a *AuthService) ValidateToken(encodedToken string) (*jwt.Token, error) {
-	return jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
-		if _, isvalid := token.Method.(*jwt.SigningMethodHMAC); !isvalid {
-			return nil, fmt.Errorf("Invalid token", token.Header["alg"])
-
+func (a *AuthService) ValidateToken(tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
-		return []byte("cilginkovboy12"), nil
+		return []byte(os.Getenv("ACCESS_SECRET")), nil
 	})
-
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
 }
